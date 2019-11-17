522794e49db7328de3a894f02f15c8e621d496d4
danil solodov <daniel.solodov@theleanapps.com>
2019-11-17 15:58:54 +0300

    Add material-icons, basic styles

--- src/app/grid-dashboard/containers/grid-dashboard/grid-dashboard.container.ts
+++ src/app/grid-dashboard/containers/grid-dashboard/grid-dashboard.container.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { GridsterConfig, GridsterItem } from 'angular-gridster2';
+import { gridOptions } from './grid-options';
 
 @Component({
   selector: 'app-grid-dashboard',
-  templateUrl: './grid-dashboard.component.html',
-  styleUrls: ['./grid-dashboard.component.css']
+  templateUrl: './grid-dashboard.container.html',
+  styleUrls: ['./grid-dashboard.container.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GridDashboardComponent implements OnInit {
+export class GridDashboardContainer implements OnInit {
 
-  constructor() { }
+  public options: GridsterConfig;
+  public dashboard: Array<GridsterItem>;
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.options = gridOptions;
+
+    this.dashboard = [
+      { cols: 2, rows: 2, y: 0, x: 0 },
+      { cols: 2, rows: 2, y: 2, x: 0 },
+    ];
   }
 
+  public changedOptions() {
+    if (this.options.api && this.options.api.optionsChanged) {
+      this.options.api.optionsChanged();
+    }
+  }
+
+  public removeItem($event, itemIndex: number) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    this.dashboard.splice(itemIndex, 1);
+  }
+
+  public addItem() {
+    this.dashboard.push({ cols: 2, rows: 2, x: 0, y: 0 });
+  }
 }
